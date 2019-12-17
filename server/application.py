d07397b0ca878d0e3c4ae63c549776fe26121338
Ofir Reuveny <ofir.reuveny@citi.com>
2019-12-17 14:11:51 +0200

    just be a pass thru

--- server/application.py
+++ server/application.py
@@ -16,8 +16,9 @@ def api_message():
 		URL = "https://orfaceapi.cognitiveservices.azure.com/face/v1.0/detect"
 		PARAMS = {'returnFaceId':'true','returnFaceAttributes':'emotion','returnFaceLandmarks':'false','recognitionModel':'recognition_01','returnRecognitionModel':'false','detectionModel':'detection_01','returnFaceId':'true'}
 		headers = {'Ocp-Apim-Subscription-Key':'386f1ee9f07a4278ba6440ad9db80580','Content-Type': 'application/octet-stream'}
-		r = requests.post(url = URL, params = PARAMS , data = request.data, headers = headers) 
-		data = r.json() 
-		return jsonify(data)
+		response = requests.post(url = URL, params = PARAMS , data = request.data, headers = headers) 
+		response.raise_for_status()
+		return response.content
 	else:
 		return "415 Unsupported Media Type ;)"
+	
